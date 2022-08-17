ce6bda1d9f127142f9f9959a872eec9a8639ebc2
Aracely Alba <aracelyalba8@gmail.com>
2022-08-16 21:34:17 -0430

    Finished code

--- main.js
+++ main.js
@@ -4,39 +4,61 @@ function random(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function options(play) {
+
+	let result = '';
+
+	if (play == 1) {
+
+		result = 'Piedra';
+
+	} else if (play == 2) {
+		
+		result = 'Papel';
+
+	} else if (play == 3) {
+		
+		result = 'Tijera';
+
+	} else {
+
+		result = 'Seleccione una opción.';
+	}
+
+	return result;
+}
+
+let pc = 0;
 let player = 0;
-let pc = random(1,3);
+let wins = 0;
+let losses = 0;
 
-player = prompt('Elige: 1 para piedra, 2 para papel, 3 para tijera.');
 
-// Opciones del jugador.
-if (player == 1) {
+while(wins < 3 && losses < 3) {
 
-	alert('Elegiste Piedra');
+	pc = random(1,3);
 
-} else if (player == 2) {
-	
-	alert('Elegiste Papel');
+	player = prompt('Elige: 1 para piedra, 2 para papel, 3 para tijera.');
 
-} else if (player == 3) {
-	
-	alert('Elegiste Tijera');
+	alert('Tú eliges: ' + options(player));
+	alert('Pc elige: ' + options(pc));
 
-} else {
 
-	alert('Seleccione una opción.');
+	// Resultados.
+	if (pc == player) {
+
+		alert('HUBO UN EMPATE.');
+
+	} else if((player == 1 && pc == 3) || (player == 2 && pc == 1) || (player == 3 && pc == 2)) {
+
+		alert('GANASTE.');
+		wins++;
+
+	} else {
+
+		alert('PERDISTE.');
+		losses++;
+	}
 }
 
-// Resultados.
-if (pc == player) {
-
-	alert('HUBO UN EMPATE.');
-
-} else if((player == 1 && pc == 3) || (player == 2 && pc == 1) || (player == 3 && pc == 2)) {
-
-	alert('GANASTE.');
-
-} else {
-
-	alert('PERDISTE');
-}
+alert(`Veces ganadas: ${wins}. Veces perdidas: ${losses}.`);
